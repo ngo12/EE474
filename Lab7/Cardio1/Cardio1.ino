30e82026d01344ebdef7bbab276226397e92d444
setsun8 <setsun8@users.noreply.github.com>
2017-02-21 15:47:12 -0800

    More button chang.es

--- Lab7/Cardio1/Cardio1.ino
+++ Lab7/Cardio1/Cardio1.ino
@@ -32,8 +32,9 @@ ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
 #define STABILITY_THRESHOLD 0.05
 #define INPUT_PIN A1
 
-#define UPPER_THRESH 2000
+#define UPPER_THRESH 3000
 #define LOWER_THRESH 1000
+#define PROG_MAX_TIME 30000 //ms
 
 const int buttonPin =  1;     // the number of the pushbutton pin.
 // Variables will change:
@@ -70,7 +71,7 @@ void setup() {
 
   // LCD Screen Setup 
   tft.begin(); 
-  tft.setRotation(1); 
+  tft.setRotation(3); 
   tft.setTextSize(2);
   tft.setTextColor(TEXT_COLOR);
   tft.fillScreen(BG_COLOR);
@@ -103,23 +104,98 @@ void setup() {
   ////Enable interrupt request
   NVIC_ENABLE_IRQ(IRQ_PDB);
 
-  stabilize2();
+//  stabilize2();
+//  tft.fillScreen(BG_COLOR);
+//  drawGrid();
+
   tft.fillScreen(BG_COLOR);
-  drawGrid();
-  
+  startScreen();
   myTimer = millis();
 }
 
 int adcValueCopy = 0;
 int x, y = 0;
 int xPrev, yPrev = 0;
+int progRunning = 0;
+unsigned long progTimer = 0;
+
 // calculated by: screen width / number of grid lines along width = pixels per grid block
 // then, 40 ms / pixels per grid block. (40ms is the length of time per grid block)
 unsigned long timePerPixel = 40 / (screenWidth / numLinesWidth);  // ms
 void loop() {
+  Serial.println("Main Loop");
 
   buttonState = onOff();
-  Serial.print(buttonState);
+  if (buttonState) {
+    Serial.println("Button On");
+    if (progRunning) {
+      Serial.println("Set Prog Running to 0");
+      progRunning = 0;
+    } else {
+      Serial.println("Set Prog Running to 1");
+      progRunning = 1;
+    }
+    buttonState = 0;
+    tft.fillScreen(BG_COLOR);
+  }
+
+  if (progRunning) {
+    if (!stabilizedFlag) {
+      Serial.println("Stabilizing..");
+      stabilize2();
+      tft.fillScreen(BG_COLOR);
+    }
+      Serial.println("EKG Prog");
+      ekgProg();     
+  } else {
+    Serial.println("Start Screen");
+    startScreen();
+    //stabilizedFlag = 0;
+  }
+
+  
+//  if (buttonState == 1) {
+//    if (!progRunning) {
+//      Serial.println("Loop1");
+//      progRunning = 1;
+//      stabilizedFlag = 0;
+//      tft.fillScreen(BG_COLOR);
+//      stabilize2();
+//      tft.fillScreen(BG_COLOR);
+//      drawGrid();
+//      progTimer = millis();
+//    } else {
+//      Serial.println("Loop2");
+//      progRunning = 0;
+//      tft.fillScreen(BG_COLOR);
+//    }
+//  } else if (buttonState == 0) {
+//    if (!progRunning) {
+//      Serial.println("Loop3");
+//      startScreen();
+//    } else {
+//      Serial.println("Loop4");
+//      ekgProg();
+//      if (millis() - progTimer >= PROG_MAX_TIME) {
+//        progRunning = 0;
+//        buttonState = 0;
+//        tft.fillScreen(BG_COLOR);
+//      }
+//    }
+//  }
+//  Serial.println("outside loops");
+
+}
+
+void startScreen() {
+
+  tft.setCursor(0, 80);
+  tft.println("EKG");
+  tft.println("Press Button to Start!");
+  delay(50);
+}
+
+void ekgProg() {
   // put your main code here, to run repeatedly:
   noInterrupts();
   adcValueCopy = adcValue;
@@ -183,7 +259,9 @@ void stabilize2() {
   int enoughDataFlag = 0;
   int bufferStableFlag = 0;
   int bufferIndex = 0;
-  while (!stabilizedFlag) {
+  buttonState = onOff();
+  while (!stabilizedFlag && buttonState != 1) {
+    Serial.print("Stable Loop");
     noInterrupts();
     valCopy = adcValue;
     interrupts();
@@ -209,7 +287,17 @@ void stabilize2() {
     if (bufferStableFlag) {
       stabilizedFlag = 1;
     }
-    tft.fillScreen(BG_COLOR);
+
+    clearTextNumbers("0000");
+    clearTextNumbers("1111");
+    clearTextNumbers("2222");
+    clearTextNumbers("3333");
+    clearTextNumbers("4444");
+    clearTextNumbers("5555");
+    clearTextNumbers("6666");
+    clearTextNumbers("7777");
+    clearTextNumbers("8888");
+    clearTextNumbers("9999");
     tft.setCursor(0, 0);
     tft.println("Stabilizing...");
     tft.print("Upper Threshold: ");
@@ -218,10 +306,23 @@ void stabilize2() {
     tft.println(LOWER_THRESH);
     tft.print("Current value: ");
     tft.println(valCopy);
-    buttonState = onOff();
-    Serial.print(buttonState);
     delay(50);
   }
+  progRunning = 0;
+}
+
+void clearTextNumbers(char* str) {
+      tft.setCursor(0, 0);
+    tft.println("Stabilizing...");
+    tft.print("Upper Threshold: ");
+    tft.println("    ");
+    tft.print("Lower Threshold: ");
+    tft.println("    ");
+    tft.print("Current value: ");
+    tft.setTextColor(BG_COLOR);
+    tft.println(str);
+    tft.setTextColor(TEXT_COLOR);
+  
 }
 
 volatile int stBufferFullFlag = 0;
@@ -310,27 +411,27 @@ void drawGrid() {
 }
 
 void addToBuffer(int val) {
-  if (stabilizedFlag) {
+//  if (stabilizedFlag) {
     myBuffer[bufferPos] = val;
     bufferPos++;
     if (bufferPos > BUFFER_SIZE) {
      bufferPos = 0;
     }
-  } else {
-    mrBuffer[mrBufferPos] = val;
-    mrBufferPos++;
-    if (mrBufferPos > MR_BUFFER_SIZE) {
-     mrBufferPos = 0;
-    }
-    stBuffer[stBufferPos] = val;
-    stBufferPos++;
-    if (stBufferPos > ST_BUFFER_SIZE) {
-     stBufferPos = 0;
-     if (stBufferFullFlag == 0) {
-       stBufferFullFlag = 1;
-     }
-    }
-  }
+//  } else {
+//    mrBuffer[mrBufferPos] = val;
+//    mrBufferPos++;
+//    if (mrBufferPos > MR_BUFFER_SIZE) {
+//     mrBufferPos = 0;
+//    }
+//    stBuffer[stBufferPos] = val;
+//    stBufferPos++;
+//    if (stBufferPos > ST_BUFFER_SIZE) {
+//     stBufferPos = 0;
+//     if (stBufferFullFlag == 0) {
+//       stBufferFullFlag = 1;
+//     }
+//    }
+//  }
 }
 
 
