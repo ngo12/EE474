10f85be12538e0166c714426efddc9ab9492da12
Ryan Linden <r.l@live.com>
2017-02-28 15:54:02 -0800

    Added PDB stuff

--- Lab8/HeartMonitor/HeartMonitor.ino
+++ Lab8/HeartMonitor/HeartMonitor.ino
@@ -26,6 +26,9 @@
 #define TFT_DC 9
 #define TFT_CS 10
 
+#define PDB_CH0C1_TOS 0x0100
+#define PDB_CH0C1_EN 0x01
+
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
 ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
 
@@ -73,6 +76,10 @@ int fileHeadingNumber = 0;
 void setup() {
 
   Serial.begin(9600);
+
+  adcInit();
+  pdbInit();
+
 
   bufferPos = 0;
   adcValue = 0;
@@ -444,4 +451,104 @@ void writeCard(int* buff, int bufferLength) {
     Serial.println("error opening file");
   }
   fileHeadingNumber++;
+}
+
+/*
+  ADC_CFG1_ADIV(2)         Divide ratio = 4 (F_BUS = 48 MHz => ADCK = 12 MHz)
+  ADC_CFG1_MODE(2)         Single ended 10 bit mode
+  ADC_CFG1_ADLSMP          Long sample time
+*/
+#define ADC_CONFIG1 (ADC_CFG1_ADIV(1) | ADC_CFG1_MODE(2) | ADC_CFG1_ADLSMP)
+
+/*
+  ADC_CFG2_MUXSEL          Select channels ADxxb
+  ADC_CFG2_ADLSTS(3)       Shortest long sample time
+*/
+#define ADC_CONFIG2 (ADC_CFG2_MUXSEL | ADC_CFG2_ADLSTS(3))
+
+void adcInit() {
+  ADC0_CFG1 = ADC_CONFIG1;
+  ADC0_CFG2 = ADC_CONFIG2;
+  // Voltage ref vcc, hardware trigger, DMA
+  ADC0_SC2 = ADC_SC2_REFSEL(0) | ADC_SC2_ADTRG | ADC_SC2_DMAEN;
+
+  // Enable averaging, 4 samples
+  ADC0_SC3 = ADC_SC3_AVGE | ADC_SC3_AVGS(0);
+
+  adcCalibrate();
+  Serial.println("calibrated");
+
+  // Enable ADC interrupt, configure pin
+  ADC0_SC1A = ADC_SC1_AIEN | channel2sc1a[3];
+  NVIC_ENABLE_IRQ(IRQ_ADC0);
+}
+
+void adcCalibrate() {
+  uint16_t sum;
+
+  // Begin calibration
+  ADC0_SC3 = ADC_SC3_CAL;
+  // Wait for calibration
+  while (ADC0_SC3 & ADC_SC3_CAL);
+
+  // Plus side gain
+  sum = ADC0_CLPS + ADC0_CLP4 + ADC0_CLP3 + ADC0_CLP2 + ADC0_CLP1 + ADC0_CLP0;
+  sum = (sum / 2) | 0x8000;
+  ADC0_PG = sum;
+
+  // Minus side gain (not used in single-ended mode)
+  sum = ADC0_CLMS + ADC0_CLM4 + ADC0_CLM3 + ADC0_CLM2 + ADC0_CLM1 + ADC0_CLM0;
+  sum = (sum / 2) | 0x8000;
+  ADC0_MG = sum;
+}
+
+/*
+  PDB_SC_TRGSEL(15)        Select software trigger
+  PDB_SC_PDBEN             PDB enable
+  PDB_SC_PDBIE             Interrupt enable
+  PDB_SC_CONT              Continuous mode
+  PDB_SC_PRESCALER(7)      Prescaler = 128
+  PDB_SC_MULT(1)           Prescaler multiplication factor = 10
+*/
+#define PDB_CONFIG (PDB_SC_TRGSEL(15) | PDB_SC_PDBEN | PDB_SC_PDBIE \
+  | PDB_SC_CONT | PDB_SC_PRESCALER(7) | PDB_SC_MULT(1))
+
+// 48 MHz / 128 / 10 / 1 Hz = 37500
+#define PDB_PERIOD (F_BUS / 128 / 10 / 1)
+
+void pdbInit() {
+  pinMode(13, OUTPUT);
+
+  // Enable PDB clock
+  SIM_SCGC6 |= SIM_SCGC6_PDB;
+  // Timer period
+  PDB0_MOD = PDB_PERIOD;
+  // Interrupt delay
+  PDB0_IDLY = 0;
+  // Enable pre-trigger
+  PDB0_CH0C1 = PDB_CH0C1_TOS | PDB_CH0C1_EN;
+  // PDB0_CH0DLY0 = 0;
+  PDB0_SC = PDB_CONFIG | PDB_SC_LDOK;
+  // Software trigger (reset and restart counter)
+  PDB0_SC |= PDB_SC_SWTRIG;
+  // Enable interrupt request
+  NVIC_ENABLE_IRQ(IRQ_PDB);
+}
+
+void adc0_isr() {
+  Serial.print("adc isr: ");
+  Serial.println(millis());
+  for (uint16_t i = 0; i < 16; i++) {
+    if (i != 0) Serial.print(", ");
+    Serial.print(samples[i]);
+  }
+  Serial.println(" ");
+}
+
+void pdb_isr() {
+  Serial.print("pdb isr: ");
+  Serial.println(millis());
+  digitalWrite(13, (ledOn = !ledOn));
+  // Clear interrupt flag
+  PDB0_SC &= ~PDB_SC_PDBIF;
 }
